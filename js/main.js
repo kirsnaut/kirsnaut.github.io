d8f3e9030969aba64a0159b96ca5465619cc0e55
Kirsten Pevidal <48729065+kapevidal@users.noreply.github.com>
2020-05-31 17:05:38 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//change navbar background when scrolling down
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
